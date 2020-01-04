2757f84a7454eb4458127c5485b519e6cf7648c0
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-01-04 20:14:56 +0100

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="pendulum",
+    version='0.0.1',
+    url='gaborvecsei.com',
+    author='Gabor Vecsei',
+    install_requires=requirements,
+    packages=find_packages(),
+)
+
